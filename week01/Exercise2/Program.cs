e31bf00a62b0e4889f0cb249ea1971e4f10b946c
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-15 21:12:35 -0600

    message

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Specialized;
 
 class Program
 {
@@ -8,25 +9,37 @@ class Program
         string userInput = Console.ReadLine();
         int grade = int.Parse(userInput);
 
+        string letter = "";
+
         if (grade >= 90)
         {
-            Console.WriteLine($"Congratulations you passed! You're grade is an A");
+            letter = "A";
         }
         else if (grade <= 89 && grade >= 80)
         {
-            Console.WriteLine($"Congratulations you passed! You're grade is an B");
+            letter = "B";
         }
         else if (grade <= 79 && grade >= 70)
         {
-            Console.WriteLine($"Congratulations you passed! You're grade is an C");
+            letter = "C";
         }
         else if (grade <= 69 && grade >= 60)
         {
-            Console.WriteLine($"Your grade is a D. I'm sorry. You didn't pass");
+            letter = "D";
         }
         else
         {
-            Console.WriteLine("You did not pass. Your grade is an F");
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try again! You can do it!");
         }
     }
 }
