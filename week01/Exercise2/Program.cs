24ddee26c33ad4821e7e02c069a8ebd6c4b9ff65
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-15 21:10:35 -0600

    changed the code to create a letter variable

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine($"Congratulations you passed! You're grade is an A");
+        }
+        else if (grade <= 89 && grade >= 80)
+        {
+            Console.WriteLine($"Congratulations you passed! You're grade is an B");
+        }
+        else if (grade <= 79 && grade >= 70)
+        {
+            Console.WriteLine($"Congratulations you passed! You're grade is an C");
+        }
+        else if (grade <= 69 && grade >= 60)
+        {
+            Console.WriteLine($"Your grade is a D. I'm sorry. You didn't pass");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Your grade is an F");
+        }
     }
 }
