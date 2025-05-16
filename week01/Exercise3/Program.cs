9dc2c68df3dc2b4f93003127dd30df1ca3321ecf
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-15 23:18:18 -0600

    finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 11);
+
+        Console.WriteLine("What is your guess?");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+        int count = 1;
+
+        while (guess != randomNumber)
+        {
+            if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+
+            Console.WriteLine("What is your guess?");
+            userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+            count++; 
+        }
+
+        Console.WriteLine("You guessed it!");
+        Console.Write($"You guessed {count} times.");
+     }
 }
