6ef96659f81a1a1d0163e4aedf5f977a0d2340c7
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-16 23:46:10 -0600

    testing

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+
+        job1.Display();
     }
 }
