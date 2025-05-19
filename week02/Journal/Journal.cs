609109eaf8da02d250400bea728f1f59f8df98a5
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-19 09:48:21 -0600

    finished exercise 1

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void Display()
+    {
+        foreach (var entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine(entry.ToFileString());
+            }
+        }
+        Console.WriteLine("Journal saved.");
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            entries.Add(Entry.FromFileString(line));
+        }
+        Console.WriteLine("Journal loaded.");
+    }
+}
