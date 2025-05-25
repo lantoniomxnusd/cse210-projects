3db193839402ba1a3a2d08d55bee45daf06d0bcd
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-24 21:01:55 -0600

    finished sandbox example from week 2

---
+++ week02/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1._firstName = "Mary";
+        p1._lastName = "Smith";
+        p1.1_age = 25;
+
+        Person p2 = new Person();
+        p2._firstName = "Johen";
+        p2._lastName = "Whatckin";
+        p2._age = 30;
+
+        list<Person> people = new List<Person>();
+        people.Add(p1);
+        people.Add(p2);
+
+        foreach (Person p in people)
+        {
+            Console.WriteLine(p.firstName);
+        }
+    }
+}
